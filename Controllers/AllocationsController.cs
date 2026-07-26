28ee98d604fa2863fb11bd244fea8462eb0ee8e6
uaj14 <an000039@algonquinlive.com>
2026-07-26 17:08:48 -0400

    Updated UI.

--- Controllers/AllocationsController.cs
+++ Controllers/AllocationsController.cs
@@ -115,57 +115,57 @@ namespace CST8256_BudgetTracker.Controllers
         }
 
         // GET: Allocations/Edit/5
-        // public async Task<IActionResult> Edit(int? id)
-        // {
-        //     if (id == null)
-        //     {
-        //         return NotFound();
-        //     }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //     var allocation = await _context.Allocations.FindAsync(id);
-        //     if (allocation == null)
-        //     {
-        //         return NotFound();
-        //     }
-        //     ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", allocation.CategoryId);
-        //     return View(allocation);
-        // }
+            var allocation = await _context.Allocations.FindAsync(id);
+            if (allocation == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", allocation.CategoryId);
+            return View(allocation);
+        }
 
         // POST: Allocations/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public async Task<IActionResult> Edit(int id, [Bind("Id,AllocationAmount,AllocationMonth,CreatedAt,UpdatedAt,CategoryId")] Allocation allocation)
-        // {
-        //     if (id != allocation.Id)
-        //     {
-        //         return NotFound();
-        //     }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AllocationAmount,AllocationMonth,CreatedAt,UpdatedAt,CategoryId")] Allocation allocation)
+        {
+            if (id != allocation.Id)
+            {
+                return NotFound();
+            }
 
-        //     if (ModelState.IsValid)
-        //     {
-        //         try
-        //         {
-        //             _context.Update(allocation);
-        //             await _context.SaveChangesAsync();
-        //         }
-        //         catch (DbUpdateConcurrencyException)
-        //         {
-        //             if (!AllocationExists(allocation.Id))
-        //             {
-        //                 return NotFound();
-        //             }
-        //             else
-        //             {
-        //                 throw;
-        //             }
-        //         }
-        //         return RedirectToAction(nameof(Index));
-        //     }
-        //     ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", allocation.CategoryId);
-        //     return View(allocation);
-        // }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(allocation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AllocationExists(allocation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", allocation.CategoryId);
+            return View(allocation);
+        }
 
         // GET: Allocations/Delete/5
         public async Task<IActionResult> Delete(int? id)
