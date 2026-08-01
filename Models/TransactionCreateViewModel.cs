f99aeae00b379d808a37123d17bca1e2fe233d70
uaj14 <an000039@algonquinlive.com>
2026-08-01 14:50:06 -0400

    Changed labels in the Transaction Create form to match the screenshots in the instructions.

--- Models/TransactionCreateViewModel.cs
+++ Models/TransactionCreateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace CST8256_BudgetTracker.Models;
 
@@ -8,9 +9,11 @@ public class TransactionCreateViewModel
     public double? Amount { get; set; }
 
     public DateOnly Date { get; set; }
-
+    
+    [Display(Name = "Type")]
     public string TransactionType { get; set; } = null!;
 
+    [Display(Name = "Category")]    // AI: Changing the text displayed in label.
     public int CategoryId { get; set; }
 
     public string? Description { get; set; } = string.Empty;
