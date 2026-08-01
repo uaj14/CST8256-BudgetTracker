e04999fa6043d69dccae30c582722988cc3bfd63
uaj14 <an000039@algonquinlive.com>
2026-08-01 17:34:20 -0400

    Added validation to Create.

--- Models/TransactionCreateViewModel.cs
+++ Models/TransactionCreateViewModel.cs
@@ -6,14 +6,20 @@ namespace CST8256_BudgetTracker.Models;
 public class TransactionCreateViewModel
 {
     public int Id { get; set; }
+    [Required]
+    [Range(0, Int32.MaxValue)]  // https://stackoverflow.com/questions/7256753/min-max-value-validators-in-asp-net-mvc
     public double? Amount { get; set; }
 
     public DateOnly Date { get; set; }
-    
+
+    [Required]
+    [AllowedValues("Expense", "Income", ErrorMessage = "Invalid transaction type.")] // AI: Direct string compare.
     [Display(Name = "Type")]
     public string TransactionType { get; set; } = null!;
 
+    [Required]
     [Display(Name = "Category")]    // AI: Changing the text displayed in label.
+    [Range(1, 10)]
     public int CategoryId { get; set; }
 
     public string? Description { get; set; } = string.Empty;
